e955d225e5d0a9ea314ab376030769b9a929ae07
AenHenIchaer <alexander_ko@inbox.ru>
2019-08-29 10:43:06 +0300

    2D Animation
    Created 2D animation using sprites

---
+++ Wave/Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexClass : MonoBehaviour
+{
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+    };
+    public const float outerRadius = 10f;
+
+    public const float innerRadius = outerRadius * 0.866025404f;
+}
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
