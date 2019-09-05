aa386f24bdc12f75e715c980315c50293aebf5e5
AenHenIchaer <alexander_ko@inbox.ru>
2019-09-05 13:38:37 +0300

    Atom (1/2 finished)

---
+++ Atom/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenspawns;
+    public float spawnDistance;
+    float timeSinceLastSpawn;
+    public Nucleon[] nucleonPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn>=timeBetweenspawns)
+        {
+            timeSinceLastSpawn -= timeBetweenspawns;
+            SpawnNucleon();
+        }
+    }
+}
