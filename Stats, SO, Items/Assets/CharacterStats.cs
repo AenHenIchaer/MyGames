913e7eb79eeba5038cb3e736afaedb70c0bca5b1
AenHenIchaer <alexander_ko@inbox.ru>
2019-11-16 10:02:37 +0300

    Inventory, potions, interactions

---
+++ Stats, SO, Items/Assets/CharacterStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }//any script can get, but only this can set
+    #region Singleton
+    public static CharacterStats instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+    private void Start()
+    {
+        currentHealth = maxHealth;//setting maxhp
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth<=0)
+        {
+            Death();
+        }
+    }
+    public void Death()
+    {
+        print("You are dead");
+    }
+    public void ModifyHealth(int healthModifier)
+    {
+        currentHealth += healthModifier;
+        print(currentHealth);
+
+    }
+}
---
+++ Stats, SO, Items/Assets/Items/HealthPotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="New Health Potion", menuName = "Inventory/Potion")]
+public class HealthPotion : Item
+{
+    public int healthModifier;
+    CharacterStats charStats;
+
+    private void Start()
+    {
+        charStats = CharacterStats.instance;
+    }
+    public override void Use()
+    {
+        base.Use();
+        ApplyEffect();
+    }
+    public void ApplyEffect()
+    {
+        CharacterStats.instance.ModifyHealth(healthModifier);
+    }
+}
