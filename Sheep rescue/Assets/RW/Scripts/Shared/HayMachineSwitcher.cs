9b00b2530987b30391ae24b09e6e00a1d38fb6a1
AenHenIchaer <alexander_ko@inbox.ru>
2019-11-25 13:46:45 +0300

    Sheep rescue (finalized)
    Sheep rescue finalized (with UI and color change for machines added)

---
+++ Sheep rescue/Assets/RW/Scripts/Shared/HayMachineSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class HayMachineSwitcher : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject blueHayMachine;
+    public GameObject yellowhayMachine;
+    public GameObject redHayMachine;
+    private int selectedIndex;
+
+public void OnPointerClick(PointerEventData eventData)
+    {
+        selectedIndex++;
+        selectedIndex %= Enum.GetValues(typeof(HayMachineColor)).Length;
+        GameSettings.hayMachineColor = (HayMachineColor)selectedIndex;
+        switch (GameSettings.hayMachineColor)
+        {
+            case HayMachineColor.Blue:
+                blueHayMachine.SetActive(true);
+                yellowhayMachine.SetActive(false);
+                redHayMachine.SetActive(false);
+                break;
+            case HayMachineColor.Yellow:
+                blueHayMachine.SetActive(false);
+                yellowhayMachine.SetActive(true);
+                redHayMachine.SetActive(false);
+                break;
+            case HayMachineColor.Red:
+                blueHayMachine.SetActive(false);
+                yellowhayMachine.SetActive(false);
+                redHayMachine.SetActive(true);
+                break;
+          
+        }
+    }
+}
